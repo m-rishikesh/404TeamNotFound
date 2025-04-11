fa805102b1b1d0c54f08e2f0773f256ee7660c8b
rajubuntu <raj@raj.com>
2025-04-12 00:24:03 +0530

    adds the features to the backend

--- backend/routes/getStock.js
+++ backend/routes/getStock.js
@@ -1,28 +1,56 @@
 import { Router } from "express";
-import {OpenAI} from "openai";
 import dotenv from "dotenv"
+import { GenerativeModel, GoogleGenerativeAI,HarmBlockThreshold,HarmCategory } from "@google/generative-ai";
+import fs from "node:fs"
+import mime from "mime-types";
 
 const router = Router()
 
 dotenv.config();
 
-const openai_key = process.env.OPENAI_KEY
+const apiKey = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+const model = genAI.getGenerativeModel(
+    {
+        model: "gemini-2.0-flash"
+    }
+)
 
-const chatai = new OpenAI({
-    apiKey : openai_key
-})
+const generativeConfig = {
+    temperature: 1,
+    topP : 0.95,
+    topK : 40,
+    maxOutputTokens: 8192,
+    responseModalities: [],
+    responseMimeType: "text/plain",
+};
 
-async function getStockData(prompt) {
-    const {prompt_content} = prompt;
-    const chat = await chatai.chat.completions.create({
-        model:"gpt-3.5-turbo",
-        messages:[
-            {role:"user",content:prompt_content}
-        ],
-        temperature:0.7
-    });
+async function run(prompt){
+    const chatSession = model.startChat({
+        generativeConfig,
+        history: [],
+    })
 
-    return chat.choices[0].message.content;
+    const result = await chatSession.sendMessage(`${prompt}`);
+    return result.response.text();
+}
+
+async function getStockData(query) {
+    const {
+        income,
+        savings,
+        debt,
+        assets,
+        investmenttimeline,
+        risktolerance,
+        investmentgoals,
+        timeofinvestment
+    } = query
+
+    const query_string = `Give me in points only what's should be my milestones and these are the factors i provide income:${income},savings:${savings},debt:${debt},assets:${assets},investment:${investmenttimeline},risktolerance:${risktolerance},investmentgoals:${investmentgoals},timeofinvestment:${timeofinvestment}`
+
+    return await run(query_string);
+
 }
 
 router.get('/',async(req,res)=>{
