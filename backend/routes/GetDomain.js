a76d2fb78b06aa445b094a0715cbe40888652710
rajubuntu <raj@raj.com>
2025-04-12 01:03:06 +0530

    updates the backend

--- backend/routes/GetDomain.js
+++ backend/routes/GetDomain.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get('/givetest',(req,res)=>{
-    res.send("from here we are going to send the test questions");
+    res.send("from here we are going to send the test questions and if there is also option to different language in frontend and if user chooses those then send also to the backend.");
 })
 
 export default router
--- backend/routes/top10ytvids.js
+++ backend/routes/top10ytvids.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {google} from "googleapis";
 import dotenv from "dotenv";
+import chatbox4yt from "./chatbox4yt.js"
 dotenv.config();
 
 const yt_api_key = process.env.YT_VIDS_API;
@@ -32,6 +33,13 @@ async function get10ytquery(query){
 router.get('/',async (req,res)=>{
     const query = req.query.q;
     res.send(await get10ytquery(query));
+})
+
+router.get('/chatquery',async (req,res)=>{
+    const {chatquery} = req.body;
+    const actual_query = `The user is learning about financial topics like Mutual Funds, SIPs, and Stock Investing.Explain the following question in very simple terms, like you're teaching a beginner.Use a friendly tone.And if the question somewhere unrelated to finance just revoke it and tell the user to ask for financial question only Question: ${chatquery}`
+    const response = await chatbox4yt(actual_query);
+    res.send(response);
 })
 
 export default router;
