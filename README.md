4acff5929fdaa2be6b4879b4532a740bce6d0937
rajubuntu <raj@raj.com>
2025-04-12 09:10:34 +0530

    updates the backend

--- README.md
+++ README.md
@@ -6,7 +6,23 @@
 
 # API Endpoints===>
  > GetDomain_API = domain_name/getDomain/givetest
+ > GetDomain_API_Chatbot = domain_name/personalizedtest (Explevel,investmentgoals,riskapetite,interestedarea,investmentstyle,marketknowedge,timeavailability,platformused,educationalbackground,languagepreferences)
  > YT_API = domain_name/top10ytquery
  > YT_CHAT_API = domain_name/top10ytquery/chatquery
- > GET_STOCK_API = domain_name/getstocks
- > GET_PRODUCT_API = domain_name/getProduct
+ > GET_STOCK_API = domain_name/getstocks {income,
+        savings,
+        debt,
+        assets,
+        investmenttimeline,
+        risktolerance,
+        investmentgoals,
+        timeofinvestment}
+ > GET_PRODUCT_API = domain_name/getProduct {
+  riskapt,
+        potentiallosses,
+        totaltime,
+        growthtime,
+        investmentrisk,
+        selectedCompanies,
+ }
+
--- backend/routes/GetDomain.js
+++ backend/routes/GetDomain.js
@@ -2,11 +2,22 @@ import { Router } from "express";
 import chatbox4yt from "./chatbox4yt.js"
 const router = Router();
 
+let personalizeddata;
+
 router.get('/givetest',async (req,res)=>{
     const {testdata} = req.body // this will take the test data.
+    const {explevel,investmentgoals,riskapetite,interestedarea,investmentstyle,marketknowledge,timeavailability,platformused,educationalbackground,languagepreferences,} = req.body
+    
+    personalizeddata = `experience level : ${explevel},investmentgoals : ${investmentgoals}, riskapetite: ${riskapetite}, investmentstyle: ${investmentstyle}, marketknowledge: ${marketknowledge}, timeavailability: ${timeavailability}, platformused : ${platformused}, educationalbackground: ${educationalbackground}, languagepreferences: ${languagepreferences} interesteddomain: ${interestedarea}`
+
     console.log(testdata)
     res.send(await chatbox4yt(testdata))
 
 })
 
+router.get('/personalizedtest',async(req,res)=>{
+    const {user_query} = req.body
+    const actual_data = personalizeddata + user_query + "give the personalized response according to the user knowledge like if you think according to the personalized data he is beginner tell him in newbie style keeping professionalism intact in the result and answer should be short as much as possible and crisp and always revoke the query if asked other than finance. "
+    res.send(await chatbox4yt(actual_data))
+})
 export default router
